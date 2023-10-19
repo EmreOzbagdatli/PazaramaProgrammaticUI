4b97bad5dc5b7a9a9723ffe0a8f8f9772f11b17d
Emre Özbağdatlı <74432853+EmreOzbagdatli@users.noreply.github.com>
2023-10-19 14:46:22 +0300

    Initial Commit

---
+++ ProgrammaticUI/ProgrammaticUI/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ProgrammaticUI
+//
+//  Created by Emre Özbağdatlı on 19.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
