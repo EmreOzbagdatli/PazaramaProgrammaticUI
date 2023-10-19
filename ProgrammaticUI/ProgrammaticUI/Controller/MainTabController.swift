427f9cd22389d4a8a44d9b6643a4a77e0a78e567
Emre Özbağdatlı <74432853+EmreOzbagdatli@users.noreply.github.com>
2023-10-19 14:53:10 +0300

    Tab Bar Controller Created

---
+++ ProgrammaticUI/ProgrammaticUI/Controller/MainTabController.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabController.swift
+//  ProgrammaticUI
+//
+//  Created by Emre Özbağdatlı on 19.10.2023.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+   
+    }
+    
+
+ 
+
+}
